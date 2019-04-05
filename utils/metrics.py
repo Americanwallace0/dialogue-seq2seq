5188102436f9375db51ec281fd82d1e0875b3090
Vincent Liu <vliu15@stanford.edu>
2019-04-04 20:29:08 -0700

    Initial working BLEU metric

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,11 +1,13 @@
 ''' This file contains all functions used to calculate evaluation metrics '''
 import torch
 import torch.nn.functional as F
+import collections
+import numpy as np
 
 import seq2seq.Constants as Constants
 
 
-def cal_performance(pred, gold, smoothing=False, mmi_factor=1.0):
+def cal_performance(pred, gold, smoothing=False, max_order=4, mmi_factor=1.0):
     '''
     Calculate accuracy and loss with
     1) label smoothing if specified
@@ -20,16 +22,19 @@ def cal_performance(pred, gold, smoothing=False, mmi_factor=1.0):
         #- Calculate CE loss with MLE objective
         loss = cal_mle_loss(pred, gold, smoothing)
         pred = pred.max(1)[1]
+
+    #- Calculate batch statistics for BLEU later
+    matches, possible_matches = cal_ngram_matches(pred.view(*gold.size()), gold, max_order=max_order)
     
     gold = gold.contiguous().view(-1)
     non_pad_mask = gold.ne(Constants.PAD)
     n_correct = pred.eq(gold)
     n_correct = n_correct.masked_select(non_pad_mask).sum().item()
-    return loss, n_correct
+    return loss, n_correct, (matches, possible_matches)
     
 def cal_mmi_loss(pred_session, pred_no_session, gold, smoothing=True, mmi_factor=1.0):
     '''
-    Calculate MMI objective, apply label smoothing if needed.
+    Calculate MMI objective, apply label smoothing if needed
 
     MMI objective:
         r* = argmax_r {log P(r|r_) - lamb * log P(r)}
@@ -57,7 +62,7 @@ def cal_mmi_loss(pred_session, pred_no_session, gold, smoothing=True, mmi_factor
 
 def cal_mle_loss(pred, gold, smoothing):
     '''
-    Calculate cross entropy loss, apply label smoothing if needed.
+    Calculate cross entropy loss, apply label smoothing if needed
     
     MLE objective:
         r* = argmax_r {log P(r|r_)}
@@ -81,3 +86,84 @@ def cal_mle_loss(pred, gold, smoothing):
         loss = F.cross_entropy(pred, gold, ignore_index=Constants.PAD, reduction='sum')
 
     return loss
+
+def cal_ngram_matches(pred, gold, max_order=4):
+    ''' Calculates n-gram matches per batch '''
+    def _get_ngrams(segment, n):
+        ''' Extracts all n-grams up to a given maximum order from an input segment '''
+        ngram_counts = collections.Counter()
+        for order in range(1, max_order + 1):
+            for i in range(0, len(segment) - order + 1):
+                ngram = tuple(segment[i:i+order])
+                ngram_counts[ngram] += 1
+        return ngram_counts
+
+    #- Set up stats tracking
+    matches_by_order = np.zeros((max_order), dtype=np.int32)
+    possible_matches_by_order = np.zeros((max_order), dtype=np.int32)
+    reference_length = 0
+    prediction_length = 0
+
+    n_insts = gold.size(0)
+
+    #- Loop through elements in batch
+    for i in range(n_insts):
+        reference = gold[i, :].squeeze(0)
+        reference = reference.masked_select(reference.ne(Constants.PAD))
+        reference = [i.item() for i in reference]
+        reference_length += len(reference)
+        prediction = pred[i, :].squeeze(0)
+        prediction = prediction.masked_select(prediction.ne(Constants.PAD))
+        prediction = [i.item() for i in prediction]
+        prediction_length += len(prediction)
+
+        #- Collect n-grams
+        reference_ngram_counts = _get_ngrams(reference, max_order)
+        prediction_ngram_counts = _get_ngrams(prediction, max_order)
+        overlap = prediction_ngram_counts & reference_ngram_counts
+
+        #- Accumulate exact n-gram matches
+        for ngram in overlap:
+            matches_by_order[len(ngram)-1] += overlap[ngram]
+
+        #- Generate total possible n-gram matches
+        for order in range(1, max_order+1):
+            possible_matches = len(prediction) - order + 1
+            if possible_matches > 0:
+                possible_matches_by_order[order-1] += possible_matches
+
+    return (matches_by_order, possible_matches_by_order)
+
+def cal_bleu_score(max_order, matches_by_order, possible_matches_by_order,
+    prediction_length, reference_length, smoothing=False):
+    ''' Calculates BLEU score given metrics accumiulated over batches '''
+    assert max_order == len(matches_by_order) == len(possible_matches_by_order)
+
+    #- Calculate precision
+    precisions = [0] * max_order
+    for i in range(0, max_order):
+        if smoothing:
+            precisions[i] = (matches_by_order[i] + 1.) / (possible_matches_by_order[i] + 1.)
+        else:
+            if possible_matches_by_order[i] > 0:
+                precisions[i] = float(matches_by_order[i]) / possible_matches_by_order[i]
+            else:
+                precisions[i] = 0.0
+
+    #- Calculate geometric mean of precisions (log-average)
+    if min(precisions) > 0:
+        p_log_sum = sum((1. / max_order) * math.log(p) for p in precisions)
+        geo_mean = math.exp(p_log_sum)
+    else:
+        geo_mean = 0
+
+    #- Calcule brevity penalty
+    ratio = float(prediction_length) / reference_length
+    if ratio >= 1.0:
+        bp = 1.
+    else:
+        bp = math.exp(1 - 1. / ratio)
+
+    bleu = geo_mean * bp
+
+    return bleu, precisions, bp, ratio
